5fb608842cb68b3323bda6477f5fae024272b219
Ihor Melnyk <73445190+Mentos302@users.noreply.github.com>
2021-10-20 17:05:52 +0300

    add category and single quering

--- server/controllers/appeal-controller.js
+++ server/controllers/appeal-controller.js
@@ -10,6 +10,30 @@ class AppealController {
       console.log(e)
     }
   }
+
+  async getByCategory(req, res) {
+    try {
+      const { category } = req.params
+
+      const appeals = await service.getByCategory(category)
+
+      res.json(appeals)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  getSingleById(req, res) {
+    try {
+      const { id } = req.params
+
+      const appeal = await service.getSingleById(id)
+
+      res.json(appeal)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 }
 
 module.exports = new AppealController()
